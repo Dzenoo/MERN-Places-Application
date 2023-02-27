b47aae8bffc716dfa0ee412c099f83c663949cd8
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-02-27 16:35:58 +0100

    Update Auth.js

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from "react";
-
 import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
